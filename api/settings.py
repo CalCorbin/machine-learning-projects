98623aefa574fd7c3302e37665722aafba94420e
Cal Corbin <ccorbin@remmac06562.local>
2022-07-15 16:22:50 -0500

    Set up endpoint for getting a sample of bear images from bing

--- api/settings.py
+++ api/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'calcorbin-ml-projects.herokuapp.com',
+    '127.0.0.1',
+    'localhost',
 ]
 
 # Application definition
--- machine_learning_projects/urls.py
+++ machine_learning_projects/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("health/", views.health, name="health"),
+    path("which-bear/", views.which_bear, name="which-bear"),
+    path("get-bear-images/", views.get_bear_images, name="get-bear-images"),
 ]
