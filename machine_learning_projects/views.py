edee550b95d1468e6d4619d93d9036c11470e8f4
Cal Corbin <ccorbin@remmac06562.local>
2022-07-15 10:29:42 -0500

    Set heroku deploy project

--- machine_learning_projects/views.py
+++ machine_learning_projects/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
-    return HttpResponse("This is Cal's machine learning API.")
+    return HttpResponse("This is Cal's machine learning API. It will host a bear classification app.")
