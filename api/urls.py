cefc13c5e529e60ff4280445dcb9d20c1f6f3965
Cal Corbin <ccorbin@remmac06562.local>
2022-07-15 09:46:08 -0500

    Create api route for ml projects

--- api/urls.py
+++ api/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('machine_learning_projects/', include('machine_learning_projects.urls')),
+    path('api/', include([
+        path('machine_learning_projects/', include('machine_learning_projects.urls')),
+        path('admin/', admin.site.urls),
+    ])),
 ]
