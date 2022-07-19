f4035752818d6e58b3c36ca02d50b33ab4a9268e
Cal Corbin <ccorbin@remmac06562.local>
2022-07-19 10:56:28 -0500

    Ensure get bear images endpoint downloads image files that exist

--- machine_learning_projects/views.py
+++ machine_learning_projects/views.py
@@ -5,6 +5,9 @@ from fastbook import *
 
 load_dotenv()
 
+bear_types = 'grizzly', 'black', 'teddy'
+path = Path('bears')
+
 
 def index(request):
     return HttpResponse("This is Cal's machine learning API. It will host a bear classification app.")
@@ -25,9 +28,24 @@ def get_bear_images(request):
     results = search_images_bing(key, "grizzly bear")
     ims = results.attrgot("contentUrl")
     image_count = len(ims)
+
+    if not path.exists():
+        path.mkdir()
+        for bear_type in bear_types:
+            dest = (path / bear_type)
+            dest.mkdir(exist_ok=True)
+            results = search_images_bing(key, f'{bear_type} bear')
+            download_images(dest, urls=results.attrgot('contentUrl'))
+
+    image_files = get_image_files(path)
+    failed = verify_images(image_files)
+
+    # Remove failed images
+    if failed:
+        failed.map(Path.unlink)
+
     response = {
         "image_count": image_count,
         "success": True,
     }
-
     return JsonResponse(response)
