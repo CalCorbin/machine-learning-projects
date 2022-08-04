840e05137ebc56202948e65b824862c398b143d2
Cal Corbin <ccorbin@remmac06562.local>
2022-08-04 14:26:12 -0500

    Return classification report from train-digits endpoint

--- machine_learning_projects/urls.py
+++ machine_learning_projects/urls.py
@@ -5,6 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("health/", views.health, name="health"),
-    path("which-bear/", views.which_bear, name="which-bear"),
-    path("get-bear-images/", views.get_bear_images, name="get-bear-images"),
+    path("train-digits/", views.train_digits, name="train-digits"),
 ]
--- machine_learning_projects/views.py
+++ machine_learning_projects/views.py
@@ -1,51 +1,49 @@
 from django.http import HttpResponse, JsonResponse
-from dotenv import load_dotenv
-from fastai.vision.widgets import *
-from fastbook import *
-
-load_dotenv()
-
-bear_types = 'grizzly', 'black', 'teddy'
-path = Path('bears')
+from sklearn import datasets, svm, metrics
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import json
 
 
 def index(request):
-    return HttpResponse("This is Cal's machine learning API. It will host a bear classification app.")
+    return HttpResponse("This is Cal's machine learning service.")
 
 
 def health(request):
     return HttpResponse("OK")
 
 
-def which_bear(request):
-    return HttpResponse("BEARS")
+def train_digits(request):
+    """
+    This function takes the digit dataset from sklearn and trains
+    a model around the data.
+    """
 
+    # Load digits and flatten the images
+    digits = datasets.load_digits()
+    n_samples = len(digits.images)
+    data = digits.images.reshape((n_samples, -1))
 
-def get_bear_images(request):
-    key = os.getenv("AZURE_SEARCH_KEY")
+    # Create a classifier: a support vector classifier
+    clf = svm.SVC(gamma=0.001)
 
-    # Create a new Azure Search client
-    results = search_images_bing(key, "grizzly bear")
-    ims = results.attrgot("contentUrl")
-    image_count = len(ims)
+    # Split data into 50% train and 50% test subsets
+    X_train, X_test, y_train, y_test = train_test_split(
+        data, digits.target, test_size=0.15, shuffle=False
+    )
 
-    if not path.exists():
-        path.mkdir()
-        for bear_type in bear_types:
-            dest = (path / bear_type)
-            dest.mkdir(exist_ok=True)
-            results = search_images_bing(key, f'{bear_type} bear')
-            download_images(dest, urls=results.attrgot('contentUrl'))
+    # Learn the digits on the train subset
+    clf.fit(X_train, y_train)
 
-    image_files = get_image_files(path)
-    failed = verify_images(image_files)
+    # Predict the value of the digit on the test subset
+    predicted = clf.predict(X_test)
 
-    # Remove failed images
-    if failed:
-        failed.map(Path.unlink)
+    # Generate a classification report
+    digits_classification_report = metrics.classification_report(
+        y_test, predicted, target_names=digits.target_names, output_dict=True
+    )
+    digits_pd_dataframe = pd.DataFrame(digits_classification_report).transpose()
+    digits_pd_dataframe = digits_pd_dataframe.to_json(orient='records', indent=2)
+    parsed = json.loads(digits_pd_dataframe)
 
-    response = {
-        "image_count": image_count,
-        "success": True,
-    }
-    return JsonResponse(response)
+    return JsonResponse({"success": True, "classification_report": parsed})
