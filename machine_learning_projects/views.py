8b2f3aee392e78ac29a7124c6f1cc3235888e482
Cal Corbin <ccorbin@remmac06562.local>
2022-08-05 11:56:59 -0500

    Add imageFile check to predict digit endpoint

--- machine_learning_projects/views.py
+++ machine_learning_projects/views.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from sklearn import datasets, svm, metrics
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import json
+
 from joblib import load
+
+trained_model_path = 'machine_learning_projects/trained_models/digits_model.joblib'
+digits_model = load('./machine_learning_projects/trained_models/digits_model.joblib')
 
 
 def index(request):
@@ -14,10 +19,24 @@ def health(request):
     return HttpResponse("OK")
 
 
+@csrf_exempt
 def predict_digit(request):
     """This endpoint is used to predict the digit in an image."""
 
-    # Load the model from the file
-    digits_model = load('./machine_learning_projects/trained_models/digits_model.joblib')
+    if request.method != 'POST':
+        return JsonResponse(
+            {'error': 'This endpoint only accepts POST requests.'},
+            status=400
+        )
+
+    if 'imageFile' not in request.FILES:
+        return JsonResponse(
+            {'error': 'Did you forget to provide a png image of a digit?'},
+            status=400
+        )
+
+    # data = json.loads(request.body)
+    # print(data)
+    # digits_model.score(X_test, y_test)
 
     return HttpResponse("OK")
