4e44b2034d272266532ae6d9fa33d70dc2a56e80
Cal Corbin <ccorbin@remmac06562.local>
2022-08-05 11:02:19 -0500

    Generate trained digit model file

--- machine_learning_projects/management/commands/makedigitsmodel.py
+++ machine_learning_projects/management/commands/makedigitsmodel.py
@@ -1,15 +1,18 @@
 from django.core.management.base import BaseCommand, CommandError
+from joblib import dump, load
 from sklearn import datasets, svm, metrics
 from sklearn.model_selection import train_test_split
 import pandas
 import json
+
+trained_model_path = 'machine_learning_projects/trained_models/digits_model.joblib'
 
 
 class Command(BaseCommand):
     help = 'Trains a model around the digits dataset'
 
     def handle(self, *args, **options):
-        self.stdout.write(self.style.SUCCESS('Training a model around the digits dataset...'))
+        self.stdout.write('\nTraining a model around the digits dataset...')
 
         # Load digits and flatten the images
         digits_dataset = datasets.load_digits()
@@ -36,7 +39,10 @@ class Command(BaseCommand):
         )
         digits_dataframe = pandas.DataFrame(digits_classification_report).transpose()
         digits_dataframe = digits_dataframe.to_json(orient='records', indent=2)
-        parsed = json.loads(digits_dataframe)
+        parsed_classification_report = json.loads(digits_dataframe)
 
-        self.stdout.write(self.style.SUCCESS('Done!'))
+        # Save the model to a file
+        dump(support_vector_classifier, trained_model_path)
+
+        self.stdout.write(self.style.SUCCESS('Done! Model saved to %s' % trained_model_path))
         return 0
